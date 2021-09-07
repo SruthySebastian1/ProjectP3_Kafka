954718b2a3f644c0f9bd72c579b12174beebdad1
Gajendrasamal6 <Gajendrasamal6@gmail.com>
2021-09-07 12:44:20 +0530

    adding 3ab

---
+++ 3ab.py
@@ -0,0 +1,32 @@
+#kafka_streaming_csv_demo.py
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+import time
+
+kafka_topic_name = "project3-topic"
+kafka_bootstrap_servers = 'localhost:9092'
+
+if __name__ == "__main__":
+    print("Stream Data Processing Application Started ...")
+    print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+    spark = SparkSession \
+        .builder \
+        .appName("PySpark Structured Streaming with Kafka and Message Format as JSON") \
+        .master("local[*]") \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    # Construct a streaming DataFrame that reads from test-topic
+    orders_df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+        .option("subscribe", kafka_topic_name) \
+        .option("startingOffsets", "latest") \
+        .load()
+
+    print("Printing Schema of orders_df: ")
+    orders_df.printSchema()
